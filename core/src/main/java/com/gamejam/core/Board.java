07edd9a283e2e51a216e79d14a66520d10cf5d9d
Mathias Robel <mathias.robel@gameforge.com>
2013-04-12 23:36:23 +0200

    added curser

--- core/src/main/java/com/gamejam/core/Board.java
+++ core/src/main/java/com/gamejam/core/Board.java
@@ -5,28 +5,39 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class Board {
-	
-	private int xSize = 5;
-	private int ySize = 5;
-	
-	Content.Name[] contents = new Content.Name[] {Content.Name.EMPTY, Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY, Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY,Content.Name.EMPTY};
-	
-	private Tile[][] tiles = new Tile[xSize][ySize];
 
-	public Board(){
-		ArrayList<Content.Name> contentList = new ArrayList<Content.Name>(Arrays.asList(contents));
-		Collections.shuffle(contentList);
-		for (int i = 0; i<xSize; i++ ){
-			for (int j = 0; j < ySize; j++){
-				Tile tile = new Tile();
-				tile.setContent(contentList.get(i + 5*j));
-				tile.setVisible(false);
-				tile.setX(i);
-				tile.setY(j);
-				tiles[i][j] = tile;
-				
-			}
-		}
-		
-	}
+    private int xSize = 5;
+    private int ySize = 5;
+    Content.Name[] contents = new Content.Name[]{Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY, Content.Name.EMPTY};
+    private Tile[][] tiles = new Tile[xSize][ySize];
+    private int currentCurserX;
+    private int currentCurserY;
+    private Tile[][] currentCurser = new Tile[xSize][ySize];
+
+    public Board() {
+        ArrayList<Content.Name> contentList = new ArrayList<Content.Name>(Arrays.asList(contents));
+        Collections.shuffle(contentList);
+        for(int i = 0; i < xSize; i++) {
+            for(int j = 0; j < ySize; j++) {
+                Tile tile = new Tile();
+                tile.setContent(contentList.get(i + 5 * j));
+                tile.setVisible(false);
+                tile.setX(i);
+                tile.setY(j);
+                tiles[i][j] = tile;
+
+            }
+        }
+        currentCurser[0][0].setVisible(true);
+    }
+
+    public void updateTiles(int x, int y, boolean visibility) {
+        currentCurserX = x;
+        currentCurserY = y;
+        tiles[currentCurserX][currentCurserY].setVisible(visibility);
+    }
+
+    public Tile getCurrentCurserTile() {
+        return tiles[currentCurserX][currentCurserY];
+    }
 }
